965ed9fd583d3934e79c6b9da09c5008e9da1ebf
JANAKARAN <jana.meganathan@oracle.com>
2026-07-14 19:08:06 +0530

    update

--- finapp-frontend/vite.config.js
+++ finapp-frontend/vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
   ],server: {
     port: 5555, // Changes localhost:5173 to localhost:port
-    host: true // Listens on all addresses, including LAN and public IPs
+    host:"0.0.0.0" ,// Listens on all available network interfaces, i mean access over internet ex: hosting in EC2 similar to host: true
+    strictPort: false //Port 5555 is already in use. The server won't start until the port is free.(if false, will change to localhost:5556)
+
+// Local:   http://localhost:5555/ -> Access the app from the same machine where Vite is running.
+// Network: http://172.31.46.243:5555/ -> private IP of your instance (like EC2 inside AWS).
   }
 })
